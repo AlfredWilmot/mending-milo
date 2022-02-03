11a9d8857aa815ef36091d86b68a6d7cdff95d46
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2022-02-03 11:50:55 +0000

    this needs to be refactored, but needed to reinstall nvidia-container-* on a test-setup to get the toolkit working-- seemed to conflict with depricated nvedia-docker2

--- docker-ros/setup-cuda-drivers-ubuntu-18-04.sh
+++ docker-ros/setup-cuda-drivers-ubuntu-18-04.sh
@@ -1,37 +1,48 @@
-# Installation cmds: https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html#ubuntu-lts
-sudo apt-get install linux-headers-$(uname -r)
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+# following this tutorial: "Workstation setup for docker with new NVIDIA container toolkit" (2019):
+# https://www.pugetsystems.com/labs/hpc/Workstation-Setup-for-Docker-with-the-New-NVIDIA-Container-Toolkit-nvidia-docker2-is-deprecated-1568/
+
+sudo apt-get remove --purge \
+    docker \
+    docker-engine \
+    docker.io \
+    containerd \
+    runc
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+
 sudo apt-get update
-sudo apt-get -y install cuda-drivers
 
-# Post install actions: https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#post-installation-actions
-export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64\
-                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+sudo apt-get install -y build-essential dkms
 
-# Starting "NVIDIA Persistence Daemon" as root (supports persistence of driver state across CUDA jobs)
-# (this seems to fail, but the drivers seem to load just fine anyway?)
-/usr/bin/nvidia-persistenced --verbose
+sudo apt-get install -y nvidia-driver-510 
 
+sudo apt-get purge -y nvidia-docker2
 
-# Verifying installation version
-cat /proc/driver/nvidia/version
-
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
 
-# Installing the NVIDIA Container Tookit: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#docker 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+&& curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list \
+&& sudo apt-get update \
+&& sudo apt-get install nvidia-container-toolkit \
+&& sudo systemctl restart docker
 
-sudo apt-get update
-sudo apt-get install -y nvidia-docker2
-sudo systemctl restart docker
 
-# verify installation by running the nvidia-smi docker container
-# NOTE: may need to restart system first
-sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
+# verify installation
+sudo docker run --gpus all --rm nvidia/cuda:11.5.0-devel-ubuntu18.04 nvidia-smi
