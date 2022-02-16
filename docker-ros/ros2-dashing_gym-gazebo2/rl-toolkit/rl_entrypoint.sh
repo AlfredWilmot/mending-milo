9b3649489174724801a31c2eded5f874dc682e35
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2022-02-16 17:44:04 +0000

    need to load ENV variables into the build file so that they can be seen at container runtime -- may be possible to address this properly using docker-compose

---
+++ docker-ros/ros2-dashing_gym-gazebo2/rl-toolkit/rl_entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e 
+
+# setup ros2 environment
+source "/opt/ros/foxy/setup.bash"
+exec "$@"
+
+# load environmental variables 
+export $(cat /.env)
+
+# MARA stuff
+source ~/ros2_mara_ws/install/setup.bash
+source /usr/share/gazebo-11/setup.sh
